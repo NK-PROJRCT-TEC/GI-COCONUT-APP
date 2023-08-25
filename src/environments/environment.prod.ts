161af5d994febf20158ed541c01c8c5835f6f560
nopporn.k <Latea@DESKTOP-N74ADFC>
2023-08-25 13:59:02 +0700

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   isMockEnabled: true,
   // API_PATH : "http://uat.ampd.co.th/forkaset-api/api",
   // API_PATH: "http://localhost:5000/api",
-  API_PATH: "http://localhost:4000/api",
+  API_PATH: "http://localhost:4001/api",
   // API_PATH : "http://gicoconutapi.nkstec.ac.th/api",
   //  API_PATH : "https://forkaset.com/forkaset-api/api",
 };
